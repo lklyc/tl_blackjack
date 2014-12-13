e7240b7eddcff4beb76269dd3ac217fd7d9f0700
Kenny Leung <kleung105@gmail.com>
2014-12-13 16:56:54 -0500

    ajax blackjacj

--- public/application.js
+++ public/application.js
@@ -1,13 +1,49 @@
 $(document).ready(function(){
-
-  $(document).on('click', '#hit_form input', function(){
-
-      $.ajax({
-        type: 'POST',
-        url: '/game/player/hit'
-      }).done(function(msg){
-        $('#game').replaceWith(msg);
-      });
-      return false;
-    });
+  player_hits();
+  player_stays();
+  dealer_hit();
 });
+
+
+
+function player_hits(){
+  
+
+    $(document).on('click', '#hit_form input', function(){
+
+        $.ajax({
+          type: 'POST',
+          url: '/game/player/hit'
+        }).done(function(msg){
+          $('#game').replaceWith(msg);
+        });
+        return false;
+      });
+
+}
+
+function player_stays(){
+    $(document).on('click', '#stay_form input', function(){
+
+        $.ajax({
+          type: 'POST',
+          url: '/game/player/stay'
+        }).done(function(msg){
+          $('#game').replaceWith(msg);
+        });
+        return false;
+      });
+}
+
+function dealer_hit(){
+    $(document).on('click', '#dealer_hit input', function(){
+
+        $.ajax({
+          type: 'POST',
+          url: '/game/dealer/hit'
+        }).done(function(msg){
+          $('#game').replaceWith(msg);
+        });
+        return false;
+      });
+}
