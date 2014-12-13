cea28395b4b264b8c22a9d6390385a5ab3ea527f
Kenny Leung <kleung105@gmail.com>
2014-12-13 15:28:50 -0500

    first commit

---
+++ public/application.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+
+  $(document).on('click', '#hit_form input', function(){
+
+      $.ajax({
+        type: 'POST',
+        url: '/game/player/hit'
+      }).done(function(msg){
+        $('#game').replaceWith(msg);
+      });
+      return false;
+    });
+});
